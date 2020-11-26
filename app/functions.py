4c165212b140a0f96cbbf56972c4d831cbb1b2f4
danielkar <daniil-karachun@mail.ru>
2020-11-27 00:52:01 +0300

    fix time #2

--- app/functions.py
+++ app/functions.py
@@ -4,7 +4,8 @@ import dbworker
 
 def check_time(chat_id):
     time_load_str = dbworker.get_field(chat_id, 'date')
-    time_load = datetime.datetime.strptime(time_load_str, '%Y-%m-%d %H:%M:%S.%f')
+    print(time_load_str)
+    time_load = datetime.strptime(time_load_str, '%Y-%m-%d %H:%M:%S.%f')
     time_now = datetime.fromtimestamp(time.time())
 
     if time_now < time_load + timedelta(weeks=1):
